af91e96b5fb05bfd9835e4451fe65817a226a352
unknown <ilasamov0204@gmail.com>
2022-10-25 16:30:34 +0400

    УУУУУУ

--- js/crap.js
+++ js/crap.js
@@ -2,5 +2,7 @@ function resizeFrame(sender){
     sender.style.height = sender.contentWindow.document.documentElement.scrollHeight + "px"
 }
 function changeContent(fileName){
+
     document.getElementById("contentFrame").src = fileName
+    resizeFrame(this)
 }
